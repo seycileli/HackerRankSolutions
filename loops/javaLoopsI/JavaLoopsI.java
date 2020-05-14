a1b5844ddab34c3000b599f6711698054da514dc
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-05-14 22:34:55 +0300

    Update JavaLoopsI.java

--- loops/javaLoopsI/JavaLoopsI.java
+++ loops/javaLoopsI/JavaLoopsI.java
@@ -9,18 +9,46 @@ public class JavaLoopsI {
         sc = new Scanner(System.in);
 
         solution();
-        
+        solutionTwo();
+        solutionThree();
     }
 
     public static void solution() {
-        System.out.println("Enter a number to multiply by i");
-        int n = sc.nextInt();
+        System.out.println("Enter a number to multiply by i"); //not needed, make sure to delete
+        int N = sc.nextInt();
 
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+        sc.close();
+    }
+
+    public static void solutionTwo() {
+        System.out.println("Enter a number to multiply by i"); //not needed, make sure to delete
+
+        int N = sc.nextInt();
+        int i = 1;
+
+        //exit i when it becomes greater than or equaled to 10
+        while (i <= 10) {
+
+            //print i in new line every time
+            System.out.println(N + " x " + i + " = " + (N * i));
+            i++; //increment i every time
+        }
+        sc.close();
+    }
+
+    public static void solutionThree() {
+        System.out.println("Enter a number to multiply by i"); //not needed, make sure to delete
+        int n = sc.nextInt();
         int result;
 
         for (int i = 1; i <= 10; i++) {
             result = n * i;
             System.out.println(n + " x " + i + " = " + result);
         }
+        sc.close();
     }
 }
